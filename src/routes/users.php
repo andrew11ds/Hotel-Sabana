ec63338e02a146252029dc576fadb16bbd3553df
Andrew <andrew11ds@hotmail.es>
2020-04-23 00:10:55 -0500

    Se ha realizado el punto #5, actualiza bien el usuario, falta mostrar el id en el punto #4

--- src/routes/users.php
+++ src/routes/users.php
@@ -3,8 +3,6 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 $app =new \Slim\App;
 
-
-
 $app ->post('/api/user/newUser',function(Request $request, Response $response){//Creación de cliente, metodo POST
   $email = $request->getParam('email');//Se hacen request de los parametros de las columnas
   $password = $request->getParam('password');
@@ -30,6 +28,44 @@ $app ->post('/api/user/newUser',function(Request $request, Response $response){/
     $resultado =null;//Db y resultado deben quedar en null cada vez que se hace un query
     $db =null;
   } catch (PDOException $e) {
+    echo '{"error" :{"text":'.$e->getMessage().'}';//Tiene error y lo muestra
+
+  }
+
+});
+
+$app ->put('/api/user/updateUser/{idUs}',function(Request $request, Response $response){//Creación de cliente, metodo POST
+  $id_user=$request->getAttribute('idUs');
+  $email = $request->getParam('email');//Se hacen request de los parametros de las columnas
+  $password = $request->getParam('password');
+  $name = $request->getParam('name');
+  $last_name= $request->getParam('last_name');
+  $address = $request->getParam('address');
+  $sql = "UPDATE users SET
+        email= :email,
+        password= :password,
+        name= :name,
+        last_name= :last_name,
+        address= :address
+        WHERE id=$id_user";
+
+  try {
+
+    $db =new db();//Se llama a la base de datos
+    $db =$db ->connectDB();
+    $resultado = $db->prepare($sql);
+    $resultado ->bindParam(':email',$email);//Se hacen bindeos al resultado para guardarlo en la base de datos
+    $resultado ->bindParam(':password',$password);
+    $resultado ->bindParam(':name',$name);
+    $resultado ->bindParam(':last_name',$last_name);
+    $resultado ->bindParam(':address',$address);
+    $resultado -> execute();
+    echo json_encode("User updated(1)");
+
+    $resultado =null;//Db y resultado deben quedar en null cada vez que se hace un query
+    $db =null;
+  } catch (PDOException $e) {
+    echo("User not updated(0)");
     echo '{"error" :{"text":'.$e->getMessage().'}';//Tiene error y lo muestra
 
   }
